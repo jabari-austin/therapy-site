43cdcabf53e5f1f5518c58c747d06a47de0be6ef
jabari-austin <austinjabari@gmail.com>
2026-03-26 18:55:08 -0400

    canva form

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+function openForm() {
+  document.getElementById("formModal").style.display = "block";}
+
+function closeForm() {
+  document.getElementById("formModal").style.display = "none";}
